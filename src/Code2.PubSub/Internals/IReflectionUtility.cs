e7385753bce7a20e92e686df75e39d08e401abf3
2code <2mail2code@gmail.com>
2023-10-25 11:39:14 +0200

    add messagebus property publish option

--- src/Code2.PubSub/Internals/IReflectionUtility.cs
+++ src/Code2.PubSub/Internals/IReflectionUtility.cs
@@ -1,10 +1,13 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 
 namespace Code2.PubSub.Internals
 {
 	internal interface IReflectionUtility
 	{
 		(Type argType, Delegate handler)? GetMessageBusPublishEventHandler(EventInfo eventInfo, IMessageBus messageBus, int channel);
+		Delegate GetMessageBusPublishAction(Type messageType, bool addChannelArg, IMessageBus messageBus);
 		void MessageBusSubscribeObjectMethod(MethodInfo methodInfo, object instance, IMessageBus messageBus, int channel);
+		
 	}
 }
