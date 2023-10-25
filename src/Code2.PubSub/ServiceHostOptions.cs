04d19cff10dd34efd287040a75a62f4a7e5bc444
2code <2mail2code@gmail.com>
2023-10-25 11:30:01 +0200

    removed "return this" for addservice methods

--- src/Code2.PubSub/ServiceHostOptions.cs
+++ src/Code2.PubSub/ServiceHostOptions.cs
@@ -1,26 +1,23 @@
-﻿namespace Code2.PubSub
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Code2.PubSub
 {
 	public class ServiceHostOptions
 	{
 		public IServiceProvider? ServiceProvider { get; set; }
 		public IMessageBus? MessageBus { get; set; }
 		public bool AutoStart { get; set; } = true;
-
 		public IList<ServiceInfo> Services { get; private set; } = new List<ServiceInfo>();
 
-		public ServiceHostOptions AddService(object instance, int channel = 0)
-		{
-			Services.Add(new ServiceInfo { Instance = instance, Channel = channel });
-			return this;
-		}
+		public void AddService(Type serviceType, int channel = 0)
+			=> Services.Add(new ServiceInfo { Type = serviceType, Channel = channel });
 
-		public ServiceHostOptions AddService(Type serviceType, int channel = 0)
-		{
-			Services.Add(new ServiceInfo { Type = serviceType, Channel = channel });
-			return this;
-		}
+		public void AddService(object instance, int channel = 0)
+			=> Services.Add(new ServiceInfo { Instance = instance, Channel = channel });
 
-		public ServiceHostOptions AddService<TService>(int channel = 0) where TService : class
+		public void AddService<TService>(int channel = 0) where TService : class
 			=> AddService(typeof(TService), channel);
 
 		public class ServiceInfo
